62d902bc20afb9510b6c1c0f8d1381cec6f8ef21
xansiety <ferando543@outlook.com>
2023-02-04 19:26:35 -0600

    Se corrige BUG de redireccionamiento

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -3,9 +3,9 @@ import { LoginPage, RegisterPage } from "../pages";
 export const AuthRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<LoginPage />} />
+      <Route index path="login" element={<LoginPage />} />
       <Route path="register" element={<RegisterPage />} />
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
+      {/* <Route path="/*" element={<Navigate to="/auth/login" />} /> */}
     </Routes>
   );
 };
